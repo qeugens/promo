4f7190bbf25370863df7ddb234157f540d67c6a1
qeugens <71491143+qeugens@users.noreply.github.com>
2021-06-23 20:59:05 +0300

    almost done

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
-  $('.slider').slick({
+  $('.slider_about').slick({
+  });
+  $('.carousel1').slick({
+    arrows:false,
+    autoplay:true,
+    infinite:true,
+    centerMode:true,
+    variableWidth: true,
+    autoplaySpeed: 1000,
+  });
+  $('.carousel2').slick({
+    arrows:false,
+    autoplay:true,
+    infinite:true,
+    centerMode:true,
+    variableWidth: true,
+    autoplaySpeed: 1000,
+  });
+  $('.carousel3').slick({
+    arrows:false,
+    autoplay:true,
+    infinite:true,
+    centerMode:true,
+    variableWidth: true,
+    autoplaySpeed: 1000,
   });
 });
